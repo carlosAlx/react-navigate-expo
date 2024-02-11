1dca34ec5c172f97811087610ca344b2ace64202
Carlos Alessandro <carllosalessandro@gmail.com>
2024-02-10 21:20:06 -0300

    feat: create structure in the project

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Feed() {
+export default function New() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Feed</Text>
+      <Text style={styles.title}>New</Text>
     </View>
   );
 }
